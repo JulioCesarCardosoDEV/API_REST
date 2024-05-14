4a0f93e8373f026207a37a53ee41dfbdcf851c5e
Júlio César <julnhoo.cesarcontreras@gmail.com>
2024-05-14 17:21:38 -0300

    Métodos Get, Delete e Post criados

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 import express from 'express';
 const app = express();
 
+// indicar para o express ler o body com json
+app.use(express.json());
+
 // mock
 const selecoes = [
     {id: 1, selecao: 'Brasil', grupo: 'G'},
     {id: 2, selecao: 'Suiça', grupo: 'G'},
-    {id: 3, selecao: 'Sérvia', grupo: 'G'},
-    {id: 4, selecao: 'Camarões', grupo: 'G'},
+    {id: 3, selecao: 'Camarões', grupo: 'G'},
+    {id: 4, selecao: 'Sérvia', grupo: 'G'}
 ];
+
+function buscarSelecaoPorId(id){
+    return selecoes.filter(selecao => selecao.id == id);
+}
+
+function buscarIndexSelecao(id){
+    return selecoes.findIndex(selecao => selecao.id == id);
+}
 
 // criar rota padrão ou raiz
 app.get('/', (req, res) => {
@@ -16,6 +27,23 @@ app.get('/', (req, res) => {
 
 app.get('/selecoes', (req, res) =>{
     res.status(200).send(selecoes);
+});
+
+app.get('/selecoes/:id', (req, res) => {
+    // let index = req.params.id;
+    res.send(buscarSelecaoPorId(req.params.id));
+});
+
+app.post('/selecoes', (req, res) => {
+    selecoes.push(req.body);
+    res.status(201).send("Seleção criada com sucesso!");
+});
+
+
+app.delete('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id);
+    selecoes.splice(index, 1);
+    res.send(`Seleção com id: ${req.params.id} excluída com sucesso`);
 });
 
 export default app;
