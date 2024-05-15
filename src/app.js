086a08871e6d67886532ed32127741d72625dc41
Júlio César <julnhoo.cesarcontreras@gmail.com>
2024-05-15 16:15:13 -0300

    Método Put criado e organização do código

--- src/app.js
+++ src/app.js
@@ -20,6 +20,11 @@ function buscarIndexSelecao(id){
     return selecoes.findIndex(selecao => selecao.id == id);
 }
 
+app.post('/selecoes', (req, res) => {
+    selecoes.push(req.body);
+    res.status(201).send("Seleção criada com sucesso!");
+});
+
 // criar rota padrão ou raiz
 app.get('/', (req, res) => {
     res.send('Node!');
@@ -34,11 +39,12 @@ app.get('/selecoes/:id', (req, res) => {
     res.send(buscarSelecaoPorId(req.params.id));
 });
 
-app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body);
-    res.status(201).send("Seleção criada com sucesso!");
+app.put('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id);
+    selecoes[index].selecao = req.body.selecao;
+    selecoes[index].grupo = req.body.grupo;
+    res.json(selecoes);
 });
-
 
 app.delete('/selecoes/:id', (req, res) => {
     let index = buscarIndexSelecao(req.params.id);
