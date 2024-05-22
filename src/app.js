afe390b0ef81c98e3344bdb0e3dd9c005882b642
Júlio César <julnhoo.cesarcontreras@gmail.com>
2024-05-21 23:19:44 -0300

    Começando a organizar o código para o padrão MVC

--- src/app.js
+++ src/app.js
@@ -4,14 +4,6 @@ const app = express();
 // indicar para o express ler o body com json
 app.use(express.json());
 
-// mock
-const selecoes = [
-    {id: 1, selecao: 'Brasil', grupo: 'G'},
-    {id: 2, selecao: 'Suiça', grupo: 'G'},
-    {id: 3, selecao: 'Camarões', grupo: 'G'},
-    {id: 4, selecao: 'Sérvia', grupo: 'G'}
-];
-
 function buscarSelecaoPorId(id){
     return selecoes.filter(selecao => selecao.id == id);
 }
@@ -20,14 +12,10 @@ function buscarIndexSelecao(id){
     return selecoes.findIndex(selecao => selecao.id == id);
 }
 
+// Rotas
 app.post('/selecoes', (req, res) => {
     selecoes.push(req.body);
     res.status(201).send("Seleção criada com sucesso!");
-});
-
-// criar rota padrão ou raiz
-app.get('/', (req, res) => {
-    res.send('Node!');
 });
 
 app.get('/selecoes', (req, res) =>{
